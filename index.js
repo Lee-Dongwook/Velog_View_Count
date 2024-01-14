52fcc11a7d97495041e870a1ee1383ffa10d3197
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-14 15:12:20 +0900

    feat : 특정 게시물 조회수 api 제작 중

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const { graphqlHTTP }  = require('express-graphql');
 
 const baseUrl = 'https://v2cdn.velog.io/graphql';
 
 const app = express();
 
-app.get('/getPosts', async (req, res) => {
+app.get('/getPostIdList', async (req, res) => {
     try {
         const response = await axios.post(baseUrl, {
             query: `
@@ -25,8 +24,9 @@ app.get('/getPosts', async (req, res) => {
         })
 
         const posts = response.data.data.posts;
-        console.log(posts);
-        res.json(posts);
+        const postIdList = posts.map((post) => post.id);
+
+        res.json(postIdList);
 
     } catch(error) {
         console.error(error);
@@ -34,14 +34,32 @@ app.get('/getPosts', async (req, res) => {
     }
 })
 
-// app.use(
-//     '/graphql',
-//     graphqlHTTP({
-//         schema: schema,
-//         rootValue: root,
-//         graphql: true,
-//     })
-// );
+app.get('/getStatusOfCertainPost', async (req, res) => {
+    try {
+      const response = await axios.post(baseUrl, {
+        query: `
+        query GetStats($post_id: ID!) {
+            getStats(post_id: $post_id) {
+                total
+                count_by_day {
+                    count
+                    day
+                }
+            }
+        }
+        `,
+        variables: {
+            post_id: "7f3fd348-8c4d-405e-a701-818586c92acf"
+        }
+      })
+
+      console.log(response.data);
+
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
